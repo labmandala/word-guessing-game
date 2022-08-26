9ff86d726144aa584a4d8282038c8701f67af971
sarah <skg@neofilm.us>
2022-08-25 20:41:36 -0400

    Add files for edit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuessesElement = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
