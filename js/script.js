e222067c7832f8f58733a5f6aeacb86db070227b
sarah <skg@neofilm.us>
2022-08-27 10:26:48 -0400

    Write function to add placeholders for each letter

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
+const word = "magnolia";
+
+// Display symbols as placeholders for the chosen word's letters
+const placeholder = function (word) {
+  const placeholderLetters = [];
+  for (const letter of word) {
+    console.log(letter);
+    placeholderLetters.push("●");
+  }
+  wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
